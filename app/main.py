901d3f56b859d1a035d6522dc604014ddaf9bc94
多啦b梦 <1715109585@qq.com>
2024-09-06 05:37:07 +0800

    1

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import sys
 import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
