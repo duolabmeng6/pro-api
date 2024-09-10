5a53268a5cf9f3c0aa4265390062c5e57d20bfbc
多啦b梦 <1715109585@qq.com>
2024-09-10 14:00:30 +0800

    1

--- app/provider/gemini/geminiProvider.py
+++ app/provider/gemini/geminiProvider.py
@@ -1,108 +1,41 @@
 import asyncio
-import os
 from typing import AsyncGenerator
-import pyefun
-from app.provider.httpxHelp import get_api_data
-from app.log import logger, error_handling
+from fastapi import HTTPException
+from app.provider.baseProvider import baseProvider
+from app.log import logger
 from app.provider.gemini.geminiSSEHandler import geminiSSEHandler as SSEHandler
 from app.provider.openaiSendBodyHeandler import openaiSendBodyHeandler
 
 
-class geminiProvider:
+class geminiProvider(baseProvider):
     def __init__(self, api_key: str, base_url: str = "https://generativelanguage.googleapis.com/v1beta"):
+        super().__init__()
         self.api_key = api_key
         self.base_url = base_url
-        self.DataHeadler = SSEHandler
-        self._debug = True
-        self._cache = True
         self.setDebugSave("openai")
+        self.DataHeadler = SSEHandler
 
-    def setDebugSave(self, name="openai"):
-        name = name.replace("/", "-")
-        # 获取当前脚本所在的目录
-        current_dir = os.path.dirname(os.path.abspath(__file__))
-        # 构造文件的绝对路径
-        self._debugfile_sse = os.path.join(current_dir + f"/debugdata/{name}_sse.txt")
-        self._debugfile_data = os.path.join(current_dir + f"/debugdata/{name}_data.txt")
-        self.debug_file = ""
-
-    async def debugRtCache(self, request):
-        self.debug_file = self._debugfile_sse if request.get("stream", False) else self._debugfile_data
-        if self._debug:
-            error = False
-            if self._cache:
-                logger.info(f"使用缓存{self.debug_file}")
-                try:
-                    data = pyefun.读入文本(self.debug_file)
-                    if not request.get("stream", False):
-                        if data != "":
-                            yield data
-
-                    arr = pyefun.分割文本(data, "\n")
-                    for line in arr:
-                        line = line.strip()
-                        if line != "":
-                            yield line
-                            error = True
-
-                except FileNotFoundError:
-                    error = False
-                    logger.warning(f"Debug file {self.debug_file} not found, it will be created in write mode.")
-            if error:
-                yield "停止"
-                return
-            if pyefun.文件是否存在(self.debug_file):
-                pyefun.删除文件(self.debug_file)
-
-    async def sendChatCompletions(self, request) -> AsyncGenerator[str, None]:
-        id = request.get('id', "")
-        model = request.get('model', "")
-        logger.name = f"openaiProvider.{id}.request.model"
-        sendReady = openaiSendBodyHeandler(self.api_key, self.base_url, model)
-        sendReady.header_openai(request)
-        pushdata = sendReady.get_Gemini()
-        url = pushdata["url"]
-        body = pushdata["body"]
-
-        #logger.info(f"\r\nsend {url} \r\nbody:\r\n{json.dumps(body, indent=4, ensure_ascii=False)}")
-
-        # 调试部分 不要看
-        async for i in self.debugRtCache(request):
-            if i == "停止":
-                return
-            else:
-                yield i
-
-        async for line in get_api_data(pushdata):
-            if self._cache:
-                if request.get("stream", False):
-                    pyefun.文件_追加文本(self.debug_file, line)
-                else:
-                    pyefun.文件_写出(self.debug_file, line)
-
-            logger.info(f"收到数据\r\n{line}")
-            yield line
 
     async def chat2api(self, request, request_model_name: str = "", id: str = "") -> AsyncGenerator[
         str, None]:
+        model = request.get('model', "")
+        logger.name = f"geminiProvider.{id}.model.{model}"
 
-        # try:
-        # except Exception as e:
-        #     import traceback
-        #     error_traceback = traceback.format_exc()
-        #     logger.error("报错了chat2api:\n%s", error_traceback)
-        #     raise HTTPException(status_code=404, detail=str(e))
+        sendReady = openaiSendBodyHeandler(self.api_key, self.base_url, model)
+        sendReady.header_openai(request)
+        pushdata = sendReady.get_Gemini()  # 改这里
 
-        with error_handling("gemini chat2api报错了"):
-            genData = self.sendChatCompletions(request)
+        try:
+            genData = self.sendChatCompletions(pushdata)
             first_chunk = await genData.__anext__()
+        except Exception as e:
+            logger.error("报错了chat2api %s", e)
+            raise HTTPException(status_code=404, detail=e)
 
         self.DataHeadler = SSEHandler(id, request_model_name)
         if not request.get("stream", False):
             content = self.DataHeadler.handle_data_line(first_chunk)
             yield content
-            #stats_data = self.DataHeadler.get_stats()
-            #logger.info(f"SSE 数据流迭代完成，统计信息：{stats_data}")
             return
 
         # 流处理的代码
@@ -118,15 +51,11 @@ class geminiProvider:
                 break
             if content:
                 yield "data: " + content
-        #stats_data = self.DataHeadler.get_stats()
-        #logger.info(f"SSE 数据流迭代完成，统计信息：{stats_data}")
-        # logger.info(f"转换为普通：{handler.generate_response()}")
-
 
 if __name__ == "__main__":
     async def main():
-        from app.apiDB import Database
-        db = Database("../../api.yaml")
+        from app.apiDB import apiDB
+        db = apiDB("../../api.yaml")
         providers, error = db.get_user_provider("sk-111111", "gemini-1.5-flash")
         provider = providers[0]
         print(provider)
@@ -141,7 +70,7 @@ if __name__ == "__main__":
             "messages": [{"role": "user", "content": "请用三句话描述春天。"}],
             "stream": False,
         }):
-            logger.info(response)
+            print(response)
 
 
     asyncio.run(main())
