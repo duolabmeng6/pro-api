db85bb16be40f197d5245cfdcd2c15b4df42b7ea
多啦b梦 <1715109585@qq.com>
2024-09-12 12:35:02 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -271,6 +271,6 @@ if __name__ == "__main__":
         "__main__:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
-        workers=1,
+        #reload=True,
+        #workers=1,
     )
